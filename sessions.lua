ae40f1f86cc2dbffc14ccd146b8eafaa3b28c2b5
xomachine <xomachiner@gmail.com>
2017-09-08 00:53:57 +0300

    Fixed many commands issues

--- sessions.lua
+++ sessions.lua
@@ -24,24 +24,30 @@ function Session:command(name, add_position)
   -- Result of the command will returned asyncroniusly
   -- via "NISGOTANSWER" event.
   local filepos = ""
+  local dirty = false
   if add_position then
-    local dirty = false
     if vis.win.file.modified then
       --save file as temporary
       dirty = os.tmpname()
       local dd = assert(io.open(dirty, "w"))
       for line in vis.win.file:lines_iterator() do
-        dd:write(line)
+        dd:write(line.."\n")
       end
+      dd:flush()
       dd:close()
     end
     local path = vis.win.file.path
     local sel = vis.win.selection
     filepos = " "..path..';'..(dirty or path)..':'..
-              tostring(sel.col)..':'..tostring(sel.line)
+              tostring(sel.line)..':'..tostring(sel.col-1)
   end
-  self.write_fd:write(name..filepos.."\n")
+  local request = name..filepos.."\n"
+  self.write_fd:write(request)
+  self.write_fd:flush() -- needed to push request forward to nimsuggest
+  --vis:message("Sent to nimsuggest: "..request)
+  os.execute("sleep 0.2") -- give nimsuggest a time to handle request
   if dirty then os.remove(dirty) end
+  self:cycle() -- read answers if any
 end
 
 function Session.new(filepath)
@@ -55,9 +61,9 @@ function Session.new(filepath)
   -- so we need to remove it first to make fifo
   local success, exitcode, signal = os.execute("mkfifo "..newtable.fifo_path)
   assert(success)
-  newtable.write_fd = assert(io.popen('bash -c "nimsuggest '..
-    newtable.file..' 2>&1 | while read n; do echo \"$n\" > '..
-    newtable.fifo_path..'; done"', 'w'))
+  newtable.write_fd = assert(io.popen('bash -c \'nimsuggest '..
+    newtable.file..' 2>&1 | while read n; do echo "$n" > '..
+    newtable.fifo_path..'; done\'', 'w'))
   newtable.read_fd = assert(io.open(newtable.fifo_path, "r"))
   return newtable
 end
@@ -66,9 +72,29 @@ function Session:cycle()
   -- Checks if nimsuggest printed the text.
   -- If so, fires NISGOTANSWERFOR:<path to related file>
   -- event with filepath
-  local result = parse_answer(self.read_fd:read("*l"))
-  if result ~= nil then
-    vis.events.emit("NISGOTANSWER", self.file, result)
+  local result = {}
+  local rawsuggestion
+  local tries = 0
+  repeat
+    rawsuggestion = self.read_fd:read("*l")
+    if rawsuggestion ~= nil then
+      --vis:message("Got answer: "..rawsuggestion)
+      tries = tries + 1
+      local suggestion = parse_answer(rawsuggestion)
+      if suggestion ~= nil then
+        table.insert(result, suggestion)
+      end
+    else
+      if tries > 10 then
+        os.execute("sleep 0.1")
+        tries = 10
+      else
+        tries = tries - 1
+      end
+    end
+  until rawsuggestion == nil and tries <= 0
+  if #result > 0 then
+    vis.events.emit("NISGOTANSWER", self.file, self.request, result)
   end
 end
 
