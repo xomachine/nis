9a0d08ed61128e964ad83dd17954eaed81321f2a
xomachine <xomachiner@gmail.com>
2017-09-09 01:44:12 +0300

    Separated graphics from logic

---
+++ graphic.lua
@@ -0,0 +1,79 @@
+local colors = {
+  Black = "\\e[30;30m",
+  Blue = "\\e[34;34m",
+  Green = "\\e[32;32m",
+  Cyan = "\\e[36;36m",
+  Red = "\\e[31;31m",
+  Purple = "\\e[35;35m",
+  Brown = "\\e[33;33m",
+  LightGray = "\\e[37;37m", -- светло-серый
+  DarkGray = "\\e[1;30m", -- тёмно-серый
+  LightBlue = "\\e[1;34m", -- светло-синий
+  LightGreen = "\\e[1;32m", -- светло-зелёный
+  LightCyan = "\\e[1;36m", -- светло-голубой
+  LightRed = "\\e[1;31m", -- светло-красный
+  LightPurple = "\\e[1;35m", -- светло-сиреневый (пурпурный)
+  Yellow = "\\e[1;33m", -- жёлтый
+  White = "\\e[1;37m", -- белый
+  NoColor = "\\e[0m", -- бесцветный
+}
+
+local suggest_glyphs = {
+  skUnknown = "{U}",
+  skAlias = " A ",
+  skConst = " C ",
+  skConverter = "C()",
+  skDynLib = "LIB",
+  skEnumField = ".EF",
+  skField = "FLD",
+  skForVar = "FOR",
+  skGenericParam = "[G]",
+  skIterator = " I ",
+  skLabel = "LBL",
+  skLet = " L ",
+  skMacro = "M[]",
+  skModule = "MOD",
+  skMethod = "M()",
+  skPackage = "PKG",
+  skParam = "(P)",
+  skProc = "P()",
+  skResult = " R ",
+  skStub = "{S}",
+  skTemp = "Tmp",
+  skTemplate = "T[]",
+  skType = " T ",
+  skVar = " V ",
+}
+
+local suggest_colors = {
+  -- Other stuff
+  skUnknown = colors.Purple,
+  skAlias = colors.Purple,
+  skDynLib = colors.Purple,
+  skPackage = colors.Purple,
+  skModule = colors.Purple,
+  skLabel = colors.Purple,
+  skType = colors.Purple,
+  skStub = colors.Purple,
+  skTemp = colors.Purple,
+  -- Fields and vars
+  skField = colors.Cyan,
+  skEnumField = colors.Cyan,
+  skForVar = colors.Cyan,
+  skLet = colors.Cyan,
+  skVar = colors.Cyan,
+  skIterator = colors.Cyan,
+  skParam = colors.Cyan,
+  skResult = colors.Cyan,
+  -- executables
+  skProc = colors.Green,
+  skConverter = colors.Green,
+  skMethod = colors.Green,
+  -- compile-time
+  skTemplate = colors.Brown,
+  skMacro = colors.Brown,
+  skConst = colors.Brown,
+  skGenericParam = colors.Brown,
+}
+
+return colors, suggest_glyphs, suggest_colors
