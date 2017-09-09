35cb1684d3d8da2b9c2727148afa392e5a2e9423
xomachine <xomachiner@gmail.com>
2017-09-09 19:12:23 +0300

    Implemented suggestions substitution

--- dispatcher.lua
+++ dispatcher.lua
@@ -4,21 +4,34 @@ local function suggest(suggestions, window)
   local file = window.file
   local pattern =
     file:content(file:text_object_word(window.selection.pos-1)) or ""
-  if pattern:match("([a-zA-Z0-9])") == nil then pattern = "" end
+  if type(pattern) ~= "string" then pattern = ""
+  elseif pattern:match("([a-zA-Z0-9])") == nil then pattern = "" end
   local variants = ""
   for _, suggestion in pairs(suggestions) do
     variants = variants..(#variants>0 and "\n" or "")..
                (graphic.associations[suggestion.skind] or "").."|"..
                (graphic.glyphs[suggestion.skind] or "U").."|"..
-               graphic.colors.LightGray.." "..suggestion.name..": "..suggestion.type
+               graphic.colors.LightGray.." "..suggestion.name..": "..
+               suggestion.type
   end
   local empty = {start = 0, finish = 0}
   local state, result, _ = vis:pipe(file, empty, "echo -e '"..variants..
                                     "' | vis-menu -l 10 '"..pattern.."'")
   if state == 0 then
-    local stripped = result:match("^.*%|[^|]+%|.*%s([^:]+):.*")
+    local stripped = result:match("^.*%|[^|]+%|[^(]*m%s([^:]+):.*")
+    if stripped == nil then stripped = result:match("%s*([^%s])%s*") end
+    local head = stripped:sub(1, #pattern)
+    if head ~= pattern then
+      --vis:message(head.." ~= "..pattern)
+      silent_print(head.." ~= "..pattern)
+      vis.win.selection.pos = vis.win.selection.pos - #pattern
+      vis:replace(head)
+      local todel = #pattern - #head
+      local pos = vis.win.selection.pos
+      vis.win.file:delete({start = pos, finish = pos + todel})
+    end
     local residue = stripped:sub(#pattern+1)
-    vis:message(tostring(residue))
+    if type(residue) == "string" and #residue > 0 then vis:insert(residue) end
   end
 end
 
